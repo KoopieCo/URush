cfd72b1aa4e8f07b0ac9871a7160d62a4100f3e4
Ryan Nolan <rynolan@uark.edu>
2017-12-04 10:08:37 -0600

    Add global config and logo resources

---
+++ app/src/main/java/com/csce4623/rynolan/urush/configurations/GlobalResources.java
@@ -0,0 +1,10 @@
+package com.csce4623.rynolan.urush.configurations;
+
+/**
+ * Created by Ryan on 12/4/2017.
+ */
+
+public class GlobalResources {
+    public static String selectedFraternity;
+    public static Integer selectedFraternityColor;
+}
